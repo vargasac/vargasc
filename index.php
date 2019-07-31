d621389afb843447f8bfb9ffc87a1c7e5a867296
vargasac <53499903+vargasac@users.noreply.github.com>
2019-07-31 09:45:21 +0800

    Rename vargasc to index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>indexfile</html>
+</head>
+<body>
+<h1>Djokovic rules, Serena falls short: What we learned at Wimbledon</h2>
+<p>LONDON -- Welcome to the secret season, the post-Wimbledon return to the clay courts -- a segment embraced some years ago by both tours partly to protect the health of the players. The prospect of moving from cushiony grass to punishing hard courts immediately after Wimbledon and staying on them through the US Open and beyond did not appeal to anyone.</p>
+<article>
+  <h1>TENNIS NEWS</h1>
+  <p>These July clay events also help balance the number of tournaments available on clay and the majority surface, hard courts. Europeans in general and clay experts in particular have fought to keep the clay tradition alive. Seven of the nine tournaments leading to the North American hard-court swing that climaxes at the US Open are on clay, including the most resonant of them all, the ATP 500 in Hamburg.
+
+So this is a good time to sit back, take a deep breath after that amazing Wimbledon men's final, and process some of the things we learned from "The Championships."
+
+</p>
+</article>
+</body>
+</html>
